96a619c10eaa3692e53eb0904c7ccc0a17ba186f
Alex Pedersen <battleriskopoly@gmail.com>
2015-03-12 20:29:02 -0400

    displaying_locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,8 @@ class Location < ActiveRecord::Base
 	belongs_to :game
 	belongs_to :user
 	has_many :trades
-
-end
+  	validates :xco, presence: true, numericality: { only_integer: true }
+	validates :yco, presence: true, numericality: { only_integer: true }
+	validates :territory, presence: true
+	validates :name, presence: true
+end  	
