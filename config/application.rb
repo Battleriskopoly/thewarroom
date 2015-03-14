c6a84f19f2ae2653c2d8c0e0d03e2e9b7cb1e07d
Alex Pedersen <battleriskopoly@gmail.com>
2015-03-14 00:03:17 -0400

    fea

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'net/http'
 
 Bundler.require(*Rails.groups)
 
-module Helpdesk
+module BR
   class Application < Rails::Application
     config.middleware.delete Rack::Lock
     config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
